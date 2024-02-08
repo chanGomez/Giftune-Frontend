e834bc0c86372c9654f31ce0e5e45748da561368
Chantal <chantalgomez@pursuit.org>
2024-02-07 19:24:43 -0500

    updating the render link for api calls

--- src/components/API/Axios.js
+++ src/components/API/Axios.js
@@ -3,7 +3,7 @@ const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8080"
-      : "https://giftune-backend.onrender.com",
+      : "https://giftune-back-end.onrender.com",
   timeout: 50000,
 });
 
